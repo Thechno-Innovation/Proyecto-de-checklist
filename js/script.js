9b36fc64a100c2c927dffeff42a462a15df43f3e
ingridpar <95387783+ingridpar@users.noreply.github.com>
2022-01-07 03:08:38 -0600

    Primer-avance-javascript

---
+++ js/script.js
@@ -0,0 +1,20 @@
+window.onload = init;
+        function init(){
+            botonEnvio = document.querySelector(".boton2");
+            nuevoItem = document.querySelector('[type="text"]');
+            listsTarea = document.getElementById("listaTarea");
+            
+            botonEnvio.addEventListener("click",anadir);
+        }
+
+        function anadir(e){
+            evento = e || window.event;
+            if (nuevoItem.value == ""){
+                evento.preventDefault();
+            }else{
+            let lista = document.createElement("li");
+            lista.innerHTML = nuevoItem.value;
+            listsTarea.appendChild(lista);
+            nuevoItem.value = "";
+            }
+        }
